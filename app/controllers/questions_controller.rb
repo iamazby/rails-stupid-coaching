49c5e6778f173e0a5322b2666584c0c9c13c6fd3
massa sakou <ams12092@gmail.com>
2020-11-16 20:04:44 -0500

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,15 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
   def answer
+    @message = params[:response]
+    if @message == 'I am going to work'
+      @response = 'Great'
+    elsif @message.include? '?'
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
   end
-
 end
